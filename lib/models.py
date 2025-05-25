651814d8754854e1d283814199c6d652b1192330
DenisMaiyo <denismaiyo14@gmail.com>
2025-05-25 23:33:29 +0300

    first push

--- lib/models.py
+++ lib/models.py
@@ -16,8 +16,19 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', backref='company')
+    devs = relationship('Dev', secondary='freebies', viewonly=True)
+
     def __repr__(self):
         return f'<Company {self.name}>'
+
+    def give_freebie(self, dev, item_name, value):
+        new_freebie = Freebie(item_name=item_name, value=value, dev=dev, company=self)
+        return new_freebie
+
+    @classmethod
+    def oldest_company(cls, session):
+        return session.query(cls).order_by(cls.founding_year).first()
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -25,5 +36,29 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', backref='dev')
+    companies = relationship('Company', secondary='freebies', viewonly=True)
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+
+    def give_away(self, dev, freebie):
+        if freebie in self.freebies:
+            freebie.dev = dev
+            return freebie
+        return None
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String(), nullable=False)
+    value = Column(Integer(), nullable=False)
+    dev_id = Column(Integer(), ForeignKey('devs.id'), nullable=False)
+    company_id = Column(Integer(), ForeignKey('companies.id'), nullable=False)
+
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
